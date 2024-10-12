017ed175a3e9a7678606960d66d3ae505310d60d
Retr0-code <nikk3695@gmail.com>
2024-10-12 10:05:18 +0300

    Small refactoring
    Moved global constats (RLC) to the top
    Moved import to the top

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -1,8 +1,14 @@
+import math as m
+
 def Amperage (U_mas):
 	I_mas = []
 	for i in range(len(U_mas)):
 		I_mas.append(round(U_mas[i]/100, 4))
 	return I_mas
+
+R = 100
+L = 6.8 * 10**(-3)
+C = 68 * 10**(-9)
 
 #1st point
 #1.3
@@ -22,7 +28,6 @@ print("3.3 I_mA: ", I_mA, "\n\n")
 
 #2nd point
 #2.1
-import math as m
 
 print("R_l: Z*_out =", round(100*(1/0.56)*m.cos(m.radians(50))-100, 3), "+ j", round(100*(1/0.56)*m.sin(m.radians(50)), 3))
 print("R_l = 14.78, X_l = 136.8")
@@ -37,18 +42,17 @@ print("Добротность конденсатора Q_c = X_c*R_c => Q_c =", 
 #3rd point
 #3.1
 
-R = 100
-C = 47 * 10**(-9)
 w = -50
 print("RC цепь")
 print("R_10 = 100 Om, пусть C = C_1 = 47 nF")
 print("ФЧХ = fi(w) = -arctg(wRC) => fi(w) =", ( -1 * m.atan( m.radians(w * R * C))))
 print("АЧХ = K(w) = 1 / sqr( 1 + (wRC)**2) => K(w) =", 1/m.sqrt(1 + (w*R*C)**2))
 
-R = 100
-L = 10 * 10**(-3)
 w = 50
 print("RL цепь")
 print("R_10 = 100 Om, пусть L = L_1 = 10 мГн")
 print("ФЧХ = fi(w) = arctg(wL/R) => fi(w) =",  m.atan( (m.radians(w*L/R)) ))
 print("АЧХ = K(w) = R * sqr( 1 + (wL/R)**2) => K(w) =", R * m.sqrt(1 + (w*L/R)**2))
+
+# 4
+
